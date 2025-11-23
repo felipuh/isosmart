97a2b2b058d26eb27b88dda1b01017f0a46cf2be
Felipe <feugalde21@outlook.com>
2025-11-23 15:24:07 -0600

    Otros ajustes de configuracion

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u7*jsa)d!to@r13ni5y_^5m!kkzy)vhh=xc@h%@#l9j8k=etd3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
