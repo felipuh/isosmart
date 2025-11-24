6129cc13a8dcfe2ec75e7d991ae901aa947e2820
Felipe <feugalde21@outlook.com>
2025-11-24 10:45:32 -0600

    frontend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -172,11 +172,21 @@ USE_TZ = True
 
 # Archivos estáticos
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/aplicacion/projects/isosmart/backend/staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Archivos media
+# Directorios adicionales de estáticos
+STATICFILES_DIRS = []
+
+# Configuración de archivos media
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/home/aplicacion/projects/isosmart/backend/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Finder de archivos estáticos
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
